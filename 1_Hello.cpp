a755cba92d754d04edba7900c1006d2b34778630
Geoffrey Lovelace <geoffrey4444@gmail.com>
2024-07-31 16:02:06 -0700

    Remove extra comment in 1_Hello.cpp

--- 1_Hello.cpp
+++ 1_Hello.cpp
@@ -12,7 +12,6 @@ int main() {
 
   constexpr double length = 3.0;
   constexpr double width{4./3.};
-  // const double distance = distance_2d(length, width);
   const auto distance = distance_2d(length, width);
   printf("%s Length: %1.4f Width: %1.2f Dist: %1.6f\n", hello.c_str(), 
          length, width, distance);
