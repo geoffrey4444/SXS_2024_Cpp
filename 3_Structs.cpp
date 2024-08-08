cdc2e82ccc9cff155d4242b7d038c23f06baffa7
Geoffrey Lovelace <geoffrey4444@gmail.com>
2024-08-08 07:35:21 -0400

    const correct structs in example 3

--- 3_Structs.cpp
+++ 3_Structs.cpp
@@ -7,24 +7,24 @@ struct RegularPolygon {
   T side_length_;
   RegularPolygon(const size_t n, const T& length)
       : number_of_sides_(n), side_length_(length) {}
-  T perimeter() { return side_length_ * number_of_sides_; }
+  T perimeter() const { return side_length_ * number_of_sides_; }
 };
 
 template <typename T>
 struct Square : RegularPolygon<T> {
   Square(const T& length) : RegularPolygon<T>(4, length) {}
-  T area() {
+  T area() const {
     return RegularPolygon<T>::side_length_ * RegularPolygon<T>::side_length_;
   }
 };
 
 int main() {
-  RegularPolygon<double> triangle{3, 4.0};
+  const RegularPolygon<double> triangle{3, 4.0};
   printf("Triangle perimeter: %1.2f\n", triangle.perimeter());
-  RegularPolygon<size_t> hex{6, 4};
+  const RegularPolygon<size_t> hex{6, 4};
   printf("Hexagon perimeter: %lu\n", hex.perimeter());
 
-  Square<double> square{8.0};
+  const Square<double> square{8.0};
   printf("Square perimeter: %1.2f area: %1.2f\n", square.perimeter(),
          square.area());
   return 0;
